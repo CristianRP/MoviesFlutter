c1ae873aea5a2f80d546a9e3ec30711f552c7b04
Cristian Ramirez <cristian.ramirez@pdc.la>
2019-07-22 15:13:18 -0600

    20190722 151318
    Added swiper for cards

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,14 +1,33 @@
 import 'package:flutter/material.dart';
 
+import 'package:movies/src/widgets/card_swiper_widget.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Text('Hello worlds'),
-      )
+        appBar: AppBar(
+          centerTitle: false,
+          title: Text('Movies in theaters'),
+          backgroundColor: Colors.indigoAccent,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
+            )
+          ],
+        ),
+        body: Container(
+            child: Column(
+          children: <Widget>[_swiperCards()],
+        )));
+  }
+
+  Widget _swiperCards() {
+    return CardSwiperWidget(
+      movies: [1,2,3,4,5],
     );
   }
 }
